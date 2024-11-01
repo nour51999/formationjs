1ec38d1d09c904a35a38a193e1f2cd2b01e3a3f0
Nour El Houda <nour51999.djouderhouda@gmail.com>
2024-11-01 21:44:38 +0100

    feet [tableaux]

--- tableaux.js
+++ tableaux.js
@@ -1,22 +1,22 @@
-// const MyArray=['banane','pomme','cerise','poire',4];
-// console.log(MyArray,"mon tableau");
-// console.log(MyArray.lenght,"la langueur");
-// console.log(MyArray.toString(),"afficher les élément en chaines de caractère");
-// console.log(MyArray.join("/"));
+const MyArray=['banane','pomme','cerise','poire',4];
+console.log(MyArray,"mon tableau");
+console.log(MyArray.lenght,"la langueur");
+console.log(MyArray.toString(),"afficher les élément en chaines de caractère");
+console.log(MyArray.join("/"));
 
-// const cars=new Array("BMW","citroan","cady");
-// console.log(cars,"Mesvoitures");
+const cars=new Array("BMW","citroan","cady");
+console.log(cars,"Mesvoitures");
 
-//  let voiture=cars.pop();//pop supprim le dernier élément du tableau
-//  console.log(voiture);//element supprimé
-//  console.log(cars);//le reste du tableau
+ let voiture=cars.pop();//pop supprim le dernier élément du tableau
+ console.log(voiture);//element supprimé
+ console.log(cars);//le reste du tableau
 
-//  let fruit=MyArray.push("kiwi");//ajouter a la liste
-//  console.log(fruit);
-//  console.log(MyArray);
-//  console.log(MyArray[1]);//Le 2émé element de la liste
-//  MyArray[0]="papaye";//écrasé la valeur banane
-//  console.log(MyArray);
+ let fruit=MyArray.push("kiwi");//ajouter a la liste
+ console.log(fruit);
+ console.log(MyArray);
+ console.log(MyArray[1]);//Le 2émé element de la liste
+ MyArray[0]="papaye";//écrasé la valeur banane
+ console.log(MyArray);
 
  
 
@@ -25,12 +25,12 @@
 //  const myChildrens=boys.Concat(girls);
 //  console.log(myChildren,"myChildren");
 
-//splice
+// //splice
 
-// const fruit=["banane","fraise"]
-//  const fr=fruit.splice(3,2);
-//  console.log(fr);
-//  console.log(fruits);
+ const fruitt=["banane","fraise","cerise","poire"];
+ fruitt.splice(2,0,"citron","kiwi");
+ 
+ console.log(fruitt);
  //slice
  const fruits2=["banane","orange","citron","pomme"];
  const slicefruit=fruits2.slice(2);
